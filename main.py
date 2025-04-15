76d4e8dc77e330ba187b4b24b3de1ac67e6d7e21
TheROoNco <suryaragavan2@outlook.com>
2025-04-15 11:07:35 +0100

    updated allowed origins

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 myApp.add_middleware(
     CORSMiddleware,
